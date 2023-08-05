6875757a33c0ce67623d50bc8c67b6ad5e493662
Koigor97 <turaysamuel101@yahoo.com>
2023-08-05 19:35:42 +0200

    Add the complete Asynchronous folder.

--- asynchronous/script.js
+++ asynchronous/script.js
@@ -3,6 +3,7 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 const url = `https://countries-api-836d.onrender.com/countries/`;
+const imageBox = document.querySelector('.images');
 
 ///////////////////////////////////////
 const renderContryInfo = function (data, className = '') {
@@ -19,7 +20,7 @@ const renderContryInfo = function (data, className = '') {
         </article>`;
 
         countriesContainer.insertAdjacentHTML('beforeend', html);
-        // countriesContainer.style.opacity = 1;
+        countriesContainer.style.opacity = 1;
 }
 
 
@@ -34,6 +35,9 @@ const getJSON = function(url, errMsg = 'Something went wrong') {
         return response.json()
     })
 }
+
+///////////////////////////////////////////////////////////////////
+// THE OLD WAY OF DOING GETTING DATA FROM AN API
 // const getCountryAndNeighbour = function (country) {
 //     // Ajax calling the first country
 //     const request = new XMLHttpRequest();
@@ -63,25 +67,133 @@ const getJSON = function(url, errMsg = 'Something went wrong') {
 
 // getCountryAndNeighbour('sierra leone');
 
-const countryData = function(country) {
-    const url = `https://restcountries.com/v2/name/${country}`;
-    getJSON(url, 'Country not found')
-    .then(data => {renderContryInfo(data[0]);
-    const neighbourCountry = data[0].borders?.[0];
+/////////////////////////////////////////////////////////////////////
+// THE MODERN WAY
+// const countryData = function(country) {
+//     const url = `https://restcountries.com/v2/name/${country}`;
+//     getJSON(url, 'Country not found')
+//     .then(data => {renderContryInfo(data[0]);
+//     const neighbourCountry = data[0].borders?.[0];
 
-    if (!neighbourCountry) throw new Error('This Country has no neighbours');
-    // country 2
-    return getJSON(neighbourCountry, 'Country not found')
-    }).then(response => response.json())
-     .then(data => renderContryInfo(data, 'neighbour'))
-    .catch(err => {
-        console.error(`Something went wrong 💥💥💥`);
-        renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`)
-    }).finally(() => {
-        countriesContainer.style.opacity = 1;
-    });
+//     if (!neighbourCountry) throw new Error('This Country has no neighbours');
+//     // country 2
+//     return getJSON(neighbourCountry, 'Country not found')
+//     }).then(response => response.json())
+//      .then(data => renderContryInfo(data, 'neighbour'))
+//     .catch(err => {
+//         console.error(`Something went wrong 💥💥💥`);
+//         renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`)
+//     }).finally(() => {
+//         countriesContainer.style.opacity = 1;
+//     });
+// }
+
+// btn.addEventListener('click', function(){
+//     countryData('iceland');
+// })
+
+///////////////////////////////////////////////////////////////
+// experimenting how the event loop works and the priority
+// the microtask qeue (for fetch function) have over the callback qeue
+// console.log('Test Start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test End..');
+// Promise.resolve('Resolved promise 2').then(res => console.log(res));
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 3').then(res => {
+//     for (let i = 0; i < 10000; i++) console.log(res)});
+
+// Building a Promise with the 'new Promise' object
+// const isLotteryWon = new Promise(function (resolve, reject) {
+//     console.log('The Lottery has begun 🔮.....');
+//     setTimeout(function () {
+//         if ((Math.random() * 100) + 1 >= 60) {
+//             resolve('You WON the Lottery 🎟️ 💵');
+//         } else reject(new Error('You LOST your money 💩'));
+//     }, 3000)
+
+// });
+
+// isLotteryWon.then(result => console.log(result)).catch(err => console.error(err));
+
+// promisifying the settimer function
+// const letsWait = function(sec) {
+//     return new Promise (resolve => setTimeout(resolve, sec * 1000))
+// }
+
+// letsWait(3).then(() => {
+//     console.log('waited for 3 sec')
+//     return letsWait(2)
+// }).then(() => console.log('waited for 2 sec'));
+
+// promisifying the geolocation
+// const getPosition = function(){
+//     console.log('Getting my position');
+//     return new Promise(function(resolve, reject) {
+//         navigator.geolocation.getCurrentPosition(resolve, reject);
+//     })
+// } 
+
+// getPosition().then(pos => console.log(pos))
+
+// //////////////////////////////////////////////////////////////////
+// THE MORE MODERN WAY
+const whereAmI = async function(country) {
+    try{
+        const response = await fetch(`https://restcountries.com/v2/name/${country}`)
+        console.log(response);
+
+        if (!response.ok) throw new Error(`Something went wrong - ${response.status}`);
+
+        const data = await response.json()
+        console.log(data[0]);
+        renderContryInfo(data[0])
+    } catch(err) {
+        console.error(err.message)
+    }
+
 }
 
-btn.addEventListener('click', function(){
-    countryData('iceland');
-})
+whereAmI('portugal')
+// console.log('I logged FIRST');
+
+// const get3CountriesAndCapitals = async function(c1, c2, c3) {
+//     try {
+        // using the .all Promise combinator
+//         const data = await Promise.all([getJSON(`https://restcountries.com/v2/name/${c1}`), getJSON(`https://restcountries.com/v2/name/${c2}`), getJSON(`https://restcountries.com/v2/name/${c3}`)])
+
+//         console.log(data);
+
+//         const capitalCities = data.map(d => d[0].capital)
+//         console.log(capitalCities);
+        
+//     } catch(err){
+//         console.log(err.message)
+//     } 
+
+// }
+
+// get3CountriesAndCapitals('canada', 'germany', 'spain') 
+
+// more Promise combinators 
+// Promise.race
+// (async function(){
+//     const whosFirst = Promise.race([getJSON(`https://restcountries.com/v2/name/canada`), getJSON(`https://restcountries.com/v2/name/france`), getJSON(`https://restcountries.com/v2/name/mexico`)]);
+
+//     console.log(whosFirst[0]);
+// })();
+
+// Promise.allSettled
+// Promise.allSettled([
+//     Promise.resolve(`It's a success`),
+//     Promise.reject(`🙁 Failed request`),
+//     Promise.resolve(`It's another success`)
+// ]).then(result => console.log(result));
+
+// Promise.any
+// Promise.any([
+//     Promise.reject(`🙁 Failed request`),
+//     Promise.resolve(`It's another success`),
+//     Promise.resolve(`It's a success`),
+// ]).then(result => console.log(result));
